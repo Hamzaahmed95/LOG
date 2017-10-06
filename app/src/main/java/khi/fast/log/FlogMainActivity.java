44efeb4f97a334ef5228b03220a87ba0223c19ae
Hamzaahmed95 <hamzzza.ahmed95@gmail.com>
2017-10-07 03:34:57 +0500

    FLOG ACTIVITY DONE!

---
+++ app/src/main/java/khi/fast/log/FlogMainActivity.java
@@ -0,0 +1,17 @@
+package khi.fast.log;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hamza Ahmed on 07-Oct-17.
+ */
+
+public class FlogMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flog_main_activity);
+    }
+}
--- app/src/main/java/khi/fast/log/SplashScreenFLOG.java
+++ app/src/main/java/khi/fast/log/SplashScreenFLOG.java
@@ -24,7 +24,7 @@ public class SplashScreenFLOG extends AppCompatActivity {
             @Override
             public void run() {
 
-                startActivity(new Intent(SplashScreenFLOG.this, FantacyLOGActivity.class));
+                startActivity(new Intent(SplashScreenFLOG.this, FlogMainActivity.class));
                 finish();
 
             }
