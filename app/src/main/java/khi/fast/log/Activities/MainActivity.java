e5cf45f34b64b1856c0c258498d067df67329e57
Hamza Ahmed <hamza.ahmed0402@gmail.com>
2020-05-22 08:04:06 +0800

    main_activity code refactored

--- app/src/main/java/khi/fast/log/Activities/MainActivity.java
+++ app/src/main/java/khi/fast/log/Activities/MainActivity.java
@@ -1,79 +1,70 @@
 package khi.fast.log.Activities;
 
-import android.app.Dialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import khi.fast.log.R;
+import khi.fast.log.Utils.Constants;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button getStarted;
+    private TextView heading;
+    private TextView sub_heading0;
+    private TextView sub_heading1;
+    private TextView sub_heading2;
+    private TextView paragraph0;
+    private TextView paragraph1;
+    private TextView paragraph2;
+    private TextView paragraph3;
+    private TextView paragraph4;
 
-    Dialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getStarted = (Button)findViewById(R.id.getStarted);
-
-        if (isFirstTime()) {
-
-            getStarted.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent i = new Intent(MainActivity.this, QuestionActivity.class);
-                    startActivity(i);
-                    finish();
-
-                }
-            });
-            if (!isTaskRoot()
-                    && getIntent().hasCategory(Intent.CATEGORY_LAUNCHER)
-                    && getIntent().getAction() != null
-                    && getIntent().getAction().equals(Intent.ACTION_MAIN)) {
-
-                finish();
-                return;
-            }
-
-            // show dialog
-        }else {
-            getStarted.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent i = new Intent(MainActivity.this, QuestionActivity.class);
-                    startActivity(i);
-
-                     finish();
-                }
-            });
-            if (!isTaskRoot()
-                    && getIntent().hasCategory(Intent.CATEGORY_LAUNCHER)
-                    && getIntent().getAction() != null
-                    && getIntent().getAction().equals(Intent.ACTION_MAIN)) {
-
-                finish();
-                return;
-            }
-        }
-
-
+        initializaiton();
+        settingValue();
+        handleClickListener();
     }
-    private boolean isFirstTime()
-    {
-        SharedPreferences preferences = this.getPreferences(MODE_PRIVATE);
-        boolean ranBefore = preferences.getBoolean("RanBefore", false);
-        if (!ranBefore) {
-            // first time
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putBoolean("RanBefore", true);
-            editor.commit();
-        }
-        return !ranBefore;
+    private void initializaiton(){
+        getStarted = (Button)findViewById(R.id.getStarted);
+        heading = (TextView)findViewById(R.id.heading);
+        sub_heading0 = (TextView)findViewById(R.id.sub_heading0);
+        sub_heading1 = (TextView)findViewById(R.id.sub_heading1);
+        sub_heading2 = (TextView)findViewById(R.id.sub_heading2);
+        paragraph0 = (TextView)findViewById(R.id.paragraph0);
+        paragraph1 = (TextView)findViewById(R.id.paragraph1);
+        paragraph2 = (TextView)findViewById(R.id.paragraph2);
+        paragraph3 = (TextView)findViewById(R.id.paragraph3);
+        paragraph4 = (TextView)findViewById(R.id.paragraph4);
+    }
+    private void settingValue(){
+        heading.setText(Constants.MAIN_ACTIVITY_HEADING);
+        sub_heading0.setText(Constants.MAIN_ACTIVITY_SUB_HEADING0);
+        sub_heading1.setText(Constants.MAIN_ACTIVITY_SUB_HEADING1);
+        sub_heading2.setText(Constants.MAIN_ACTIVITY_SUB_HEADING2);
+        paragraph0.setText(Constants.MAIN_ACTIVITY_PARAGRAPH0);
+        paragraph1.setText(Constants.MAIN_ACTIVITY_PARAGRAPH1);
+        paragraph2.setText(Constants.MAIN_ACTIVITY_PARAGRAPH2);
+        paragraph3.setText(Constants.MAIN_ACTIVITY_PARAGRAPH3);
+        paragraph4.setText(Constants.MAIN_ACTIVITY_PARAGRAPH4);
+        getStarted.setText(Constants.MAIN_ACTIVITY_BUTTON_TEXT);
+    }
+    private void handleClickListener(){
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, QuestionActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
     }
 }
--- app/src/main/java/khi/fast/log/Utils/Constants.java
+++ app/src/main/java/khi/fast/log/Utils/Constants.java
@@ -1,4 +1,16 @@
 package khi.fast.log.Utils;
 
 public class Constants {
+
+    public static final String MAIN_ACTIVITY_HEADING = "WINNERS DON'T WAIT FOR CHANCES";
+    public static final String MAIN_ACTIVITY_SUB_HEADING0 = "THEY";
+    public static final String MAIN_ACTIVITY_SUB_HEADING1 = "MAKE ";
+    public static final String MAIN_ACTIVITY_SUB_HEADING2 = "THEM";
+    public static final String MAIN_ACTIVITY_PARAGRAPH0 = "\"O";
+    public static final String MAIN_ACTIVITY_PARAGRAPH1 = "T ";
+    public static final String MAIN_ACTIVITY_PARAGRAPH2 = "EAM, ";
+    public static final String MAIN_ACTIVITY_PARAGRAPH3 = "D";
+    public static final String MAIN_ACTIVITY_PARAGRAPH4 = "REAM\"";
+    public static final String MAIN_ACTIVITY_BUTTON_TEXT = "Get it on!";
+
 }
